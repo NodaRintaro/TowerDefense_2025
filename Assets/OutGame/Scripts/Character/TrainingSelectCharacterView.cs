6debb4f696c6aa3f74ae700bf4ebaa79cc92f530
Enulu <enuarle8@icloud.com>
2025-12-20 03:23:31 +0900

    [Fix]キャラクター選択画面の修正

---
+++ Assets/OutGame/Scripts/Character/TrainingSelectCharacterView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 育成ゲームのキャラクター選択画面で選択中のキャラクターのView
+/// </summary>
+public class TrainingSelectCharacterView : CharacterViewBase
+{
+    [SerializeField] Image _roleImage;
+
+    [SerializeField] TMP_Text _nameText;
+
+    public void SetRole(Sprite roleSprite)
+    {
+        _roleImage.sprite = roleSprite;
+    }
+
+    public void SetName(string name)
+    {
+        _nameText.text = name; 
+    }
+}

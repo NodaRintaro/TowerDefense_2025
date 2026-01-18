c1fbd7c8e40e8730fe1e083d7bd8babd4ff5f749
Yusuke17 <yuusuke.k1717@gmail.com>
2026-01-18 21:12:18 +0900

    [fix]ScreenQuickFadeOutを直した

--- Assets/OutGame/GameCommon/Scripts/Screen/ScreenQuickFadeOut.cs
+++ Assets/OutGame/GameCommon/Scripts/Screen/ScreenQuickFadeOut.cs
@@ -1,34 +1,43 @@
 ﻿using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// データのロードを行わないスクリーン変更用の簡易的なフェードアウト
 /// </summary>
 public class ScreenQuickFadeOut : MonoBehaviour
 {
-    [SerializeField] private RectTransform currentScreen; // 現在の画面
-    [SerializeField] private RectTransform nextScreen;    // 次の画面
-    [SerializeField] private CanvasGroup blurOverlay;     // ぼかし用のパネル
+    [SerializeField] private Image blurOverlay;     // ぼかし用のパネル
 
     public void ChangeScreen(RectTransform currentScreen, RectTransform nextScreen)
     {
         float screenWidth = Screen.width;
 
         // 次の画面をあらかじめ右側に待機させる
-        nextScreen.transform.position = new Vector2(screenWidth, 0);
+        currentScreen.gameObject.SetActive(true);
+        nextScreen.gameObject.SetActive(true);
+        nextScreen.anchoredPosition = new Vector2(screenWidth, 0);
 
         // 1. 現在の画面を左へ
-        currentScreen.DOAnchorPos(new Vector2(-screenWidth, 0), 0.5f).SetEase(Ease.InCubic);
+        currentScreen.DOAnchorPos(new Vector2(-screenWidth, 0), 0.5f).SetEase(Ease.InCubic).OnComplete(()=> currentScreen.gameObject.SetActive(false));
 
         // 2. 次の画面を中央へ
-        nextScreen.DOAnchorPos(Vector2.zero, 0.5f).SetEase(Ease.OutCubic);
+        nextScreen.DOAnchorPos(Vector2.zero, 0.5f).SetEase(Ease.InCubic);
 
         // 3. ぼかしパネルをサッと出して消す
-        blurOverlay.alpha = 0;
-        blurOverlay.DOFade(1, 0.25f).OnComplete(() => {
-            blurOverlay.DOFade(0, 0.25f);
+        blurOverlay.gameObject.SetActive(true);
+        blurOverlay.DOFade(0f, 0f);
+        blurOverlay.DOFade(1, 0.5f).OnComplete(() => {
+            blurOverlay.DOFade(0, 0.5f).OnComplete(() =>
+            {
+                blurOverlay.gameObject.SetActive(false);
+            });
         });
     }
+    
+    
 }

50f869d6dcaa660795fe35909734b3ba1ee03b7e
GotouTamaki <tamagoro0226@gmail.com>
2026-01-20 23:55:46 +0900

    [add]スキルデータそのものの作成、スキルデータをダウンロードできるように追加

---
+++ Assets/OutGame/GameCommon/Scripts/AAGAddress/AAGSkillData.cs
@@ -0,0 +1,6 @@
+﻿// 自動生成のソースコードです
+public class AAGSkillData
+{
+    public const string kAssets_MasterData_ScriptableObject_SkillData_SkillData = "Assets/MasterData/ScriptableObject/SkillData/SkillData.asset";
+
+}
---
+++ Assets/OutGame/GameCommon/Scripts/Data/Skill/DurationType.cs
@@ -0,0 +1,8 @@
+public enum DurationType
+{
+    None = 0,
+    Duration,
+    Instant,
+    Endless,
+    Passive,
+}

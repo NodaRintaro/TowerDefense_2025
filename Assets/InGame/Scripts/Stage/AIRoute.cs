339435948f43cbfde975e1d59cf3f7e28a382eac
HiyoshiTurio <turio01217@gmail.com>
2026-01-22 00:58:32 +0900

    no message

--- Assets/InGame/Scripts/Stage/AIRoute.cs
+++ Assets/InGame/Scripts/Stage/AIRoute.cs
@@ -13,12 +13,15 @@ public class AIRoute// : ScriptableObject
     }
     public void RemoveAtPoint(int index)
     {
-        if (points.Length <= 0 || index < 0 || index >= points.Length) return;
+        if (points.Length <= 0 || index < 0 || index > points.Length) return;
+        Debug.Log("RemovePoint");
         Vector3[] vecs = new Vector3[points.Length - 1];
         for (int i = 0, j = 0; i < vecs.Length; i++, j++)
         {
             if (j == index) j++;
             vecs[i] = points[j];
         }
+
+        points = vecs;
     }
 }
